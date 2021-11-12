969e73c74567abc8f48834847153f3fc6194ce03
sohan110111 <sohan110111@gmail.com>
2021-11-13 00:36:50 +0600

    input form keyboard

---
+++ InputOutput.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // cout << "Please input amount " << endl;
+
+    int amount1;
+    int amount2;
+    
+    cout << "Plese input amount1 : ";
+    cin >> amount1;
+
+    cout << "Plese input amount2 : ";
+    cin >> amount2;
+
+    int sum = amount1 + amount2;
+
+    cout << "Total Amount is : " << sum << endl;
+
+    return 0;
+}
+
+
+// w3schools code
+/*#include <iostream>
+using namespace std;
+
+int main() {
+  int x;
+  cout << "Type a number: "; // Type a number and press enter
+  cin >> x; // Get user input from the keyboard
+  cout << "Your number is: " << x;
+  return 0;
+} */
