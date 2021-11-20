c422c47aeceb9492d0e12074ce69b529ca2c64cb
sohan110111 <sohan110111@gmail.com>
2021-11-20 23:53:11 +0600

    Operators discussion

---
+++ Operators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number1, number2;
+    cout << "Please enter number1 : ";
+    cin >> number1;
+
+    cout << "Please enter number2 : ";
+    cin >> number2;
+
+    int sum = number1 + number2;
+    int subtraction = number1 - number2;
+    int multiplication = number1 * number2;
+    int division = number1 / number2;
+    int increment = number1++;
+    int decrement = number2--;
+    int module = number1 % number2;
+
+    cout << "Sum = " << sum << "; Subtraction : " << subtraction << "; Multiplication : " << multiplication << "; Division : " << division << "; Increment : " << increment << "; Decrement : "<< decrement << "; Reminder : " << module;
+}
